214eabffd7b8e337e6f2f02a9cae5b51a089ea84
Dean Guo <396901990@qq.com>
2016-09-20 15:38:38 +0800

    add base file

---
+++ app/src/main/java/com/dean/articlecomment/base/BaseActivity.java
@@ -0,0 +1,59 @@
+package com.dean.articlecomment.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by codeest on 2016/8/2.
+ * MVP activity基类
+ */
+public abstract class BaseActivity<T extends BasePresenter> extends AppCompatActivity implements BaseView{
+
+    protected T mPresenter;
+    protected Activity mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+        mContext = this;
+        if (mPresenter != null)
+            mPresenter.attachView(this);
+        App.getInstance().addActivity(this);
+        init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void setToolBar(Toolbar toolbar, String title) {
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPresenter != null)
+            mPresenter.detachView();
+        App.getInstance().removeActivity(this);
+    }
+
+    protected abstract int getLayoutId();
+    protected abstract void init();
+}
---
+++ app/src/main/java/com/dean/articlecomment/base/BaseFragment.java
@@ -0,0 +1,66 @@
+package com.dean.articlecomment.base;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by codeest on 2016/8/2.
+ * MVP Fragment基类
+ */
+public abstract class BaseFragment<T extends BasePresenter> extends Fragment implements BaseView{
+
+    protected T mPresenter;
+    protected View mView;
+    protected Activity mActivity;
+    protected Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        mActivity = (Activity) context;
+        mContext = context;
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(getLayoutId(), null);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPresenter.attachView(this);
+        ButterKnife.bind(this, view);
+        init();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mPresenter != null)
+            mPresenter.detachView();
+    }
+
+    protected abstract int getLayoutId();
+    protected abstract void init();
+}
